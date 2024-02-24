72fdf122e5a3ccdba7da76ceb85dc5af1e2d5053
mprokopchuk <mprokopchuk@apple.com>
2024-02-24 15:34:55 -0800

    bash scripts

---
+++ dist.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+APP_NAME=alpha4
+
+DIST_DIR="./dist"
+MISC_DIR="./misc"
+
+rm -irf "${DIST_DIR}"
+
+mkdir "${DIST_DIR}"
+
+cp ./target/${APP_NAME}*-jar-with-dependencies.jar "${DIST_DIR}"
+cp ${MISC_DIR}/*.sh ${DIST_DIR}
+cp ${MISC_DIR}/*.service ${DIST_DIR}
+cp ${MISC_DIR}/*.properties ${DIST_DIR}
---
+++ uninstall.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+APP_NAME=alpha4
+USER=jouda
+GROUP=jouds
+
+APP_DIR="/usr/share/${APP_NAME}"
+SVC_DIR=/etc/systemd/system
+
+echo "Stopping service before removal"
+{
+	systemctl stop "${APP_NAME}"
+} || {
+	echo "Failed to stop previous service version"
+}
+
+rm "${SVC_DIR}/${APP_NAME}"
+rm -rf "${APP_DIR}"
+
+
+echo "Refreshing systemd services"
+systemctl daemon-reload
+systemctl reset-failed
