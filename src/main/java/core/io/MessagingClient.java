e737f5a819c8ec6179f8fc0da2fa31299933091f
mprokopchuk <mprokopchuk@apple.com>
2024-02-24 19:00:57 -0800

    added timeout to the socket

--- src/main/java/core/io/MessagingClient.java
+++ src/main/java/core/io/MessagingClient.java
@@ -16,6 +16,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -33,6 +34,8 @@ import java.util.concurrent.Future;
 public class MessagingClient implements Runnable, Config {
 
     private static final Logger log = LoggerFactory.getLogger(MessagingClient.class);
+
+    private static final int SOCKET_TIMEOUT = 1000;
 
     private final MessagesManager msgMgr;
     private final PeerManager peerMgr;
@@ -77,9 +80,15 @@ public class MessagingClient implements Runnable, Config {
                 Future<?> future = executor.submit(() -> {
                     log.debug("Executing messaging receive for peer {} {}:{}", peerId, ipAddress, port);
                     // handshake
-                    try (Socket socket = new Socket(ipAddress, port);
-                            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));) {
+                    Socket socket = null;
+                    PrintWriter out = null;
+                    BufferedReader in = null;
+                    try {
+                        socket = new Socket();
+                        socket.connect(new InetSocketAddress(ipAddress, port), SOCKET_TIMEOUT);
+                        out = new PrintWriter(socket.getOutputStream(), true);
+                        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
                         // send message to the peer
                         out.println(commandString);
                         out.println();
@@ -99,6 +108,19 @@ public class MessagingClient implements Runnable, Config {
                         String msg = String.format("Failed to send command %s to the peer %s %s:%s", commandString,
                                 peerId, ipAddress, port);
                         log.error(msg, e);
+                    } finally {
+                        try {
+                            socket.close();
+                        } catch (Exception e) {
+                        }
+                        try {
+                            out.close();
+                        } catch (Exception e) {
+                        }
+                        try {
+                            in.close();
+                        } catch (Exception e) {
+                        }
                     }
                     // sending new messages if any
                     Map<Long, Message> newMsgs = msgMgr.getNewMessages();
@@ -114,10 +136,12 @@ public class MessagingClient implements Runnable, Config {
                             continue;
                         }
                         log.debug("Sending new message {} to peer {} {}:{}", messageId, peerId, ipAddress, port);
-                        try (Socket socket = new Socket(ipAddress, port);
-                                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                                BufferedReader in = new BufferedReader(
-                                        new InputStreamReader(socket.getInputStream()));) {
+                        try {
+                            socket = new Socket();
+                            socket.connect(new InetSocketAddress(ipAddress, port), SOCKET_TIMEOUT);
+                            out = new PrintWriter(socket.getOutputStream(), true);
+                            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
                             String requestString = JsonUtil
                                     .toJson(new Request(Command.NEW_MESSAGE, this.peerId, messageId, message));
                             out.println(requestString);
@@ -129,6 +153,19 @@ public class MessagingClient implements Runnable, Config {
                             String msg = String.format("Failed to send command %s to the peer %s %s:%s", commandString,
                                     peerId, ipAddress, port);
                             log.error(msg, e);
+                        } finally {
+                            try {
+                                socket.close();
+                            } catch (Exception e) {
+                            }
+                            try {
+                                out.close();
+                            } catch (Exception e) {
+                            }
+                            try {
+                                in.close();
+                            } catch (Exception e) {
+                            }
                         }
                     }
 
